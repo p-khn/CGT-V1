50390d60264f1c35da12741e0bb289f20ccc446f
P K <114946477+p-khn@users.noreply.github.com>
2026-05-11 12:56:23 +0100

    update train

--- train.py
+++ train.py
@@ -4,3 +4,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+
+from model import gaussian_nll
+
+def create_optimizer(model, cfg):
+    return optim.Adam(model.parameters(), lr=cfg.lr)
