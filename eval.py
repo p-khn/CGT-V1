5b9b5e5fb67cb855340b2a63343352ed75441e09
P K <114946477+p-khn@users.noreply.github.com>
2026-06-15 18:24:44 +0100

    update eval

--- eval.py
+++ eval.py
@@ -4,3 +4,15 @@ import numpy as np
 import pandas as pd
 import torch
 from sklearn.metrics import average_precision_score, roc_auc_score
+from attribution import (
+    _build_feature_window,
+    get_ground_truth_dims,
+    hit_att,
+    ndcg_att,
+    parse_external_labels,
+    score_with_matrix_shadow,
+)
+from data import make_loader
+from model import gaussian_nll
+from spot_methods import SPOT
+from utils import _aggregate, adjust_predicts, apply_stress, calc_point2point
