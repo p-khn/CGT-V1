d9e72f292d7f97ed730e87799c9ed36ed7dab32e
P K <114946477+p-khn@users.noreply.github.com>
2026-08-27 11:49:10 +0100

    update attr

--- attribution.py
+++ attribution.py
@@ -4,3 +4,20 @@ from sklearn.metrics import ndcg_score
 
 from model import gaussian_nll
 from utils import _aggregate
+
+def parse_external_labels(filepath, D, one_based=True):
+    labels = []
+    with open(filepath, "r") as f:
+        for line in f:
+            line = line.strip()
+            if not line:
+                continue
+            interval, dims_str = line.split(":")
+            start, end = map(int, interval.split("-"))
+            raw_dims = [int(x.strip()) for x in dims_str.split(",") if x.strip() != ""]
+            if one_based:
+                raw_dims = [d - 1 for d in raw_dims]
+            dims = {d for d in raw_dims if 0 <= d < D}
+            labels.append({"start": start, "end": end, "dims": dims})
+    return labels
+
